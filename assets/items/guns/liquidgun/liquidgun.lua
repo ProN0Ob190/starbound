61e01646aa4e07c1f7879d90ee6afa04a752afb8
Ryan Brady <rdbrady@gmail.com>
2014-08-06 04:46:36 -0700

    nightly Wed 08/06/2014

--- assets/items/guns/liquidgun/liquidgun.lua
+++ assets/items/guns/liquidgun/liquidgun.lua
@@ -1,16 +1,16 @@
 function init()
-  self.fireInterval = 0.1
+  self.fireInterval = 0.2
   self.fireTimer = self.fireInterval * 0.5
   self.minLiquidLevel = 230
   self.range = 15
   self.dropItems = {
-    [1] = "ice", --water
-    [2] = "ice", --endless water
-    [3] = "magmarock", --lava
-    [5] = "magmarock", --endless lava
-    [4] = "dirtmaterial", --acid (or poison if you prefer)
-    [6] = "fleshblock", --tentacle juice
-    [7] = "tar"  --tar
+    [1] = "liquidwater", --water
+    [2] = "liquidwater", --endless water
+    [3] = "liquidlava", --lava
+    [5] = "liquidlava", --endless lava
+    [4] = "liquidacid", --acid (or poison if you prefer)
+    [6] = "liquidtentaclejuice", --tentacle juice
+    [7] = "liquidoil"  --oil
   }
 end
 
@@ -20,7 +20,8 @@ end
 
 function continueFire(dt)
   self.fireTimer = self.fireTimer - dt
-  if self.fireTimer <= 0 and suckLiquid() then
+  if self.fireTimer <= 0 then
+    suckLiquid()
     self.fireTimer = self.fireInterval
   end
 end
@@ -28,37 +29,43 @@ end
 function suckLiquid()
   local tarPos = item.ownerAimPosition()
   local srcPos = item.ownerPosition()
-  if world.magnitude(tarPos, srcPos) > self.range or world.lineCollision(srcPos, tarPos) then
+  local range = world.magnitude(tarPos, srcPos)
+  if range > self.range or world.lineCollision(srcPos, tarPos) then
     return false
   end
 
   -- target tile plus tiles to right and left
   local checkTiles = {
     tarPos,
-    {tarPos[1] + 1, tarPos[2]},
-    {tarPos[1] - 1, tarPos[2]}
+    vec2.add(tarPos, {1, 0}),
+    vec2.add(tarPos, {-1, 0})
   }
 
-  -- try to drain tiles within range until we have a full unit (super wasteful!)
+  -- drain tiles and drop items (VERY approximate)
   local totalLevel = 0
   local liquidId = 0
+  local didSuck = false
   for i, pos in ipairs(checkTiles) do
     local liquid = world.visibleLiquidAt(pos)
     if liquid and self.dropItems[liquid[1]] and (liquidId == 0 or liquid[1] == liquidId) then
       liquidId = liquid[1]
       totalLevel = totalLevel + liquid[2]
-      if totalLevel >= self.minLiquidLevel then
+      while totalLevel >= self.minLiquidLevel do
+        totalLevel = totalLevel - self.minLiquidLevel
         while i > 0 do
           world.destroyLiquid(checkTiles[i])
           i = i - 1
         end
         world.spawnItem(self.dropItems[liquidId], srcPos)
-        return true
+        didSuck = true
       end
     end
   end
 
-  return false
+  local direction = world.distance(srcPos, tarPos)
+  world.spawnProjectile("vacuum", tarPos, 0, direction, false, { speed = math.max(range - 3, 0) * 2 })
+
+  return didSuck
 end
 
 function startTriggered() end
